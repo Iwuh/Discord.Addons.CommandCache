2a94cb8024303d823ba22cf01e20c732a5e23a69
Matthew Faigan <matthew.faigan@gmail.com>
2017-04-16 22:06:55 -0400

    make example bot

--- src/ExampleBot/Program.cs
+++ src/ExampleBot/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Discord;
+using Discord.Addons.CommandCache;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace ExampleBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private DependencyMap _map;
+        private CommandService _commands;
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Hello World!");
+            _map = new DependencyMap();
+
+            _client = new DiscordSocketClient().UseCommandCache(_map, 200, Log);
+            _map.Add(_client);
+
+            _commands = new CommandService();
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.Log += Log;
+            _client.MessageReceived += HandleCommand;
+
+            await _client.LoginAsync(TokenType.Bot, "YOUR TOKEN HERE");
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleCommand(SocketMessage message)
+        {
+            var userMessage = message as SocketUserMessage;
+            if (userMessage == null) return;
+            if (userMessage.Author.IsBot) return;
+
+            int argPos = 0;
+            if (userMessage.HasCharPrefix('+', ref argPos))
+            {
+                var context = new CommandContext(_client, userMessage);
+                await _commands.ExecuteAsync(context, argPos, _map);
+            }
         }
     }
 }
---
+++ src/ExampleBot/TestModule.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Discord.Addons.CommandCache;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleBot
+{
+    public class TestModule : ModuleBase
+    {
+        private CommandCacheService _cache;
+
+        public TestModule(CommandCacheService ccs)
+        {
+            _cache = ccs;
+        }
+
+        [Command("add")]
+        [Summary("Adds two whole numbers together.")]
+        public async Task Add(int first, int second)
+        {
+            // Deleting the command message will automatically delete the response message sent by the line below.
+            await Context.Message.Channel.SendCachedMessageAsync(_cache, Context.Message.Id, $"{first} plus {second} is {first + second}.");
+        }
+
+        [Command("info")]
+        [Summary("Gets information about the bot.")]
+        [Alias("stats")]
+        public async Task GetInfo()
+        {
+            var embed = new EmbedBuilder()
+                .WithTitle("Bot stats:")
+                .AddField(f => f.WithName("Heap size:").WithValue($"{GC.GetTotalMemory(false) / 1024.0f / 1024.0f} MB").WithIsInline(true))
+                .AddField(f => f.WithName("Discord.Net Version:").WithValue(DiscordConfig.Version).WithIsInline(true))
+                .AddField(f => f.WithName("Total Guilds:").WithValue((Context.Client as DiscordSocketClient).Guilds.Count).WithIsInline(true));
+
+            var message = await ReplyAsync(string.Empty, embed: embed);
+            _cache.Add(Context.Message.Id, message.Id);
+        }
+    }
+}
