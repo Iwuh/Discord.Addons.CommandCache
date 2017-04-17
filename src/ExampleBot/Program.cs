5d529bf77fc0a5a8dd45d422aa1a8e9b3ea94d71
Matthew Faigan <matthew.faigan@gmail.com>
2017-04-16 21:37:06 -0400

    create example bot project, update packages

---
+++ src/ExampleBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
